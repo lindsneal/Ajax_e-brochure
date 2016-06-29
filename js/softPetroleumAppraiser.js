11da89034a94b0772bd18538537da5f782aa1645
Lindsay <neal1191@gmail.com>
2016-06-28 21:38:20 -0500

    initial project version

---
+++ js/softPetroleumAppraiser.js
@@ -0,0 +1,53 @@
+
+/*function loadAjaxPackage(url) {
+	$('#bigBox >') //Select all children of bigBox article element
+		.fadeOut(500)	//fade out selection.
+		
+		//run the queue of actions (steps) to replace content in particular section
+		.queue(function(){ 
+			//Note: the reference to #bigBox in the "load(url + #bigBox > *' ...)" section is the element tag from the source page to be copied, 
+			//while the other references to #bigBox are for the destination element tag to be loaded in the current page.
+			console.log('source to load from: ' + url + ' #bigBox > *');
+			$('#bigBox').load(url + ' #bigBox > *', function(){
+				$('#bigBox >').hide()
+				.fadeIn(500)
+				.dequeue(); 
+			});
+		});	
+}
+*/
+function pageAjax(url, id) {
+	$(id + ' >')
+		.fadeOut(800)
+		.queue(function(){ 
+			$(id).load(url + ' ' + id + ' > *', function(){
+				$(id + ' >').hide().fadeIn(800).dequeue(); 
+			});
+				//this brings the start point back to the top of page.//
+				$("html, body").animate({ scrollTop: 0 }, 200); 
+		});	
+}
+
+
+$(document).ready(function(){
+
+
+	$('body').on('click', '#bigBox a', function(e){
+		var url = $(this).attr('href');
+		var id = '#' + $(this).parents().attr('id');
+		pageAjax(url, id);
+		e.preventDefault();
+	});	
+
+
+	/* jQuery ajax implementation brings in new article package on click event of accordion list */
+	/*$('#bigBox a').on('click', function(e){
+			var url = $(this).attr('href');
+			console.log('clicked: ' + url);
+			loadAjaxPackage(url);
+			e.preventDefault();
+		});	*/	
+
+
+
+});
